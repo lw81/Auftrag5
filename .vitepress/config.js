fa1d943625aec1906e40fa617bc7fa8c7db6431d
lw81 <113902927+lw81@users.noreply.github.com>
2023-07-12 12:11:07 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/Auftrag5/",
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
   themeConfig: {
